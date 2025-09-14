a3ca3a281433eea80b972d6a7e53be7e133a4de1
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-09-14 10:59:32 +1000

    https://github.com/mP1/walkingkooka/pull/2964 ImmutableList.setElements(Collection) was List
    - https://github.com/mP1/walkingkooka/pull/2964
    - ImmutableList.setElements(Collection) was List

--- src/main/java/walkingkooka/math/NumberList.java
+++ src/main/java/walkingkooka/math/NumberList.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package walkingkooka.math;
 
 import walkingkooka.collect.list.ImmutableListDefaults;
 import walkingkooka.collect.list.Lists;
 
 import java.util.AbstractList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,7 +41,7 @@ public final class NumberList extends AbstractList<Number>
     /**
      * Factory that creates a {@link NumberList} from the list of {@link Number numbers}.
      */
-    public static NumberList with(final List<Number> numbers) {
+    public static NumberList with(final Collection<Number> numbers) {
         Objects.requireNonNull(numbers, "numbers");
 
         NumberList DateList;
@@ -71,7 +89,7 @@ public final class NumberList extends AbstractList<Number>
     }
 
     @Override
-    public NumberList setElements(final List<Number> numbers) {
+    public NumberList setElements(final Collection<Number> numbers) {
         final NumberList copy = with(numbers);
         return this.equals(copy) ?
             this :
