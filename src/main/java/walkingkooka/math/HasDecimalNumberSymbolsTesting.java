9d1db3b1f4882523d041e43a275254385e4b73b9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-23 12:26:17 +1000

    HasDecimalNumberSymbolsTesting.DIFFERENT_OPTIONAL_DECIMAL_NUMBER_SYMBOLS

--- src/main/java/walkingkooka/math/HasDecimalNumberSymbolsTesting.java
+++ src/main/java/walkingkooka/math/HasDecimalNumberSymbolsTesting.java
@@ -38,6 +38,8 @@ public interface HasDecimalNumberSymbolsTesting extends TreePrintableTesting,
         new DecimalFormatSymbols(DIFFERENT_LOCALE)
     );
 
+    Optional<DecimalNumberSymbols> DIFFERENT_OPTIONAL_DECIMAL_NUMBER_SYMBOLS = Optional.of(DIFFERENT_DECIMAL_NUMBER_SYMBOLS);
+
     default void decimalNumberSymbolsAndCheck(final HasDecimalNumberSymbols has,
                                               final DecimalNumberSymbols expected) {
         this.checkEquals(
